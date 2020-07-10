0d7cc28abd9c9883dfc7db6e76b1c5f5acbc5921
pponte81 <68053465+pponte81@users.noreply.github.com>
2020-07-10 12:41:18 +0200

    Update generar_tabla.py

--- generar_tabla.py
+++ generar_tabla.py
@@ -8,20 +8,20 @@ def formato_tabla(filename):
     while i < len(xls.sheet_names):
         hoja = xls.sheet_names[i]
         df = pd.read_excel(filename, sheet_name=hoja)
+        df = df.applymap(str)
         df.to_excel(writer, index=False, sheet_name=hoja)
         wb = writer.book
         ws = writer.sheets[hoja]
         col_names = [{'header': col_name} for col_name in df.columns]
-        ws.add_table(0, 0, df.shape[0], df.shape[1] - 1, {'columns': col_names})
-        longitudes_campos = [max([len(row) for row in df[df.columns[y]].dropna()]) for y in range(len(df.columns))]
+        ws.add_table(0, 0, df.shape[0], df.shape[1]-1, {'columns': col_names})
+        longitudes_campos = [max([len(row) for row in df[df.columns[y]]]) for y in range(len(df.columns))]
         longitudes_nombres = [len(col) for col in df.columns]
         longitudes_max = list(zip(longitudes_campos, longitudes_nombres))
         longitudes_finales = [max(i) for i in longitudes_max]
         for z, width in enumerate(longitudes_finales):
             ws.set_column(z, z, width)
-        i += 1
+        i +=1
 
     writer.save()
     writer.close()
     print("Fichero con formato tabla generado")
-
