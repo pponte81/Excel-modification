da5ccb5db4a161465b31781a3fae066f3b0f35b3
pponte81 <68053465+pponte81@users.noreply.github.com>
2020-08-05 09:58:46 +0200

    Update generar_tabla.py

--- generar_tabla.py
+++ generar_tabla.py
@@ -10,16 +10,17 @@ def formato_tabla(filename):
         df = pd.read_excel(filename, sheet_name=hoja)
         df = df.applymap(str)
         df.to_excel(writer, index=False, sheet_name=hoja)
-        wb = writer.book
-        ws = writer.sheets[hoja]
-        col_names = [{'header': col_name} for col_name in df.columns]
-        ws.add_table(0, 0, df.shape[0], df.shape[1]-1, {'columns': col_names})
-        longitudes_campos = [max([len(row) for row in df[df.columns[y]]]) for y in range(len(df.columns))]
-        longitudes_nombres = [len(col)+4 for col in df.columns]
-        longitudes_max = list(zip(longitudes_campos, longitudes_nombres))
-        longitudes_finales = [max(i) for i in longitudes_max]
-        for z, width in enumerate(longitudes_finales):
-            ws.set_column(z, z, width)
+        if df.shape[0] > 0:
+            wb = writer.book
+            ws = writer.sheets[hoja]
+            col_names = [{'header': col_name} for col_name in df.columns]
+            ws.add_table(0, 0, df.shape[0], df.shape[1]-1, {'columns': col_names})
+            longitudes_campos = [max([len(row) for row in df[df.columns[y]]]) for y in range(len(df.columns))]
+            longitudes_nombres = [len(col)+4 for col in df.columns]
+            longitudes_max = list(zip(longitudes_campos, longitudes_nombres))
+            longitudes_finales = [max(i) for i in longitudes_max]
+            for idx, width in enumerate(longitudes_finales):
+                ws.set_column(idx, idx, width)
         i +=1
 
     writer.save()
